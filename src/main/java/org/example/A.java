425eceba321dc3c94a055f1529ac04e0f9f944c0
pushpraj <pushprjanghore@gmail.com>
2025-12-14 20:56:10 +0530

    Revert "Update A.java"
    This reverts commit c993acb218366136ae9161c8236216ae406f3ac7.

--- src/main/java/org/example/A.java
+++ src/main/java/org/example/A.java
@@ -4,7 +4,5 @@ public class A {
     public static void main(String[] args) {
         System.out.println("ss");
         String s ="Hello recieved";
-
-        sssssssssssssssssssssssssssssssssssssss
     }
 }
